7a262fb459b5e97472dd5ee7fa8af190f4708b27
Mateusz Grochowski <groszek20027@gmail.com>
2024-03-10 23:25:07 +0100

    Working draft

--- run.py
+++ run.py
@@ -11,12 +11,14 @@ event = Event()
 event.add('summary', 'Spotkanie 1')
 event.add('dtstart', datetime(2024, 3, 15, 10, 0, 0, tzinfo=pytz.utc))
 event.add('dtend', datetime(2024, 3, 15, 12, 0, 0, tzinfo=pytz.utc))
-event.add('dtstamp', datetime(2024, 3, 15, 10, 0, 0, tzinfo=pytz.utc))
+event.add('dtstamp', datetime(2024, 3, 15, 0, 10, 0, tzinfo=pytz.utc))
 
 cal.add_component(event)
 
-directory = tempfile.mkdtemp()
-f = open(os.path.join(directory, 'example.ics'), 'wb')
+path = r"./"
+filename = r"example.ics"
+
+f = open(os.path.join(path, filename), 'wb')
 f.write(cal.to_ical())
 f.close()
 

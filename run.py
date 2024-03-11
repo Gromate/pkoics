c0722525dad1f7383b675a4c3db2c0fd6b775be6
Mateusz Grochowski <groszek20027@gmail.com>
2024-03-11 11:01:49 +0100

    Moved api call to schedule class

--- run.py
+++ run.py
@@ -1,30 +1,12 @@
 from icalendar import Calendar, Event
 from datetime import datetime
 from schedule.schedule import Schedule
-import os
-import requests
-import json 
-import pytz
-
-url = 'https://schedulescraperpw.azurewebsites.net/api/schedule?TeamName=13K1&LabGropName=L01&ProjGroupName=P01&CompLabGroupName=K01&WeekType=P'
-
-api_response = requests.get(url)
-if api_response.status_code == 200:
-    print('Success')
-parsed_json = json.loads(api_response.text)
-
-print(parsed_json[0]['name'])
 
 schedule = Schedule('13K1')
 schedule.init_calendar()
 
-event = Event()
-event.add('summary', 'Spotkanie 2')
-event.add('dtstart', datetime(2024, 3, 15, 10, 0, 0, tzinfo=pytz.utc))
-event.add('dtend', datetime(2024, 3, 15, 12, 0, 0, tzinfo=pytz.utc))
-event.add('dtstamp', datetime(2024, 3, 15, 0, 10, 0, tzinfo=pytz.utc))
-event.add('RRULE', {'FREQ': 'WEEKLY', 'INTERVAL': 2, 'COUNT': 3})
+schedule.get_schedule_from_api()
 
-schedule.cal.add_component(event)
+schedule.add_lesson()
 
 schedule.save_calendar_to_file()
--- schedule/schedule.py
+++ schedule/schedule.py
@@ -1,5 +1,9 @@
 from icalendar import Calendar, Event
+from datetime import datetime
+import requests
+import json 
 import os
+import pytz
 
 class Schedule:
     def __init__(self, team_name):
@@ -7,12 +11,49 @@ class Schedule:
 
     def init_calendar(self):
         cal = Calendar()
-        cal.add('prodid', 'MyCal')
+
+        cal_description = f'Plan zajęć {self.team_name}'
+        cal.add('prodid', cal_description)
         cal.add('version', '1.0')
+
         self.cal = cal
 
     def save_calendar_to_file(self, path='./', filename='example.ics'):
         f = open(os.path.join(path, filename), 'wb')
         f.write(self.cal.to_ical())
         f.close()
+
+    def add_lesson(self):
+        event = Event()
+        event.add('summary', 'Spotkanie 2')
+        event.add('dtstart', datetime(2024, 3, 15, 10, 0, 0, tzinfo=pytz.utc))
+        event.add('dtend', datetime(2024, 3, 15, 12, 0, 0, tzinfo=pytz.utc))
+        event.add('dtstamp', datetime(2024, 3, 15, 0, 10, 0, tzinfo=pytz.utc))
+        event.add('RRULE', {'FREQ': 'WEEKLY', 'INTERVAL': 2, 'COUNT': 3})
+
+        self.cal.add_component(event)
+
+    def get_schedule_from_api(self):
+        base_url = 'https://schedulescraperpw.azurewebsites.net/api/schedule?'
+        url = ''.join([
+            base_url,
+            f'TeamName={self.team_name}',
+            f'&LabGropName=L01',
+            f'&ProjGroupName=P01',
+            f'&CompLabGroupName=K01',
+            f'&WeekType=P'
+        ])
+
+        api_response = requests.get(url)
+        parsed_json = json.loads(api_response.text)
+        self.__check_status_code(api_response)
+
+        print(parsed_json[0]['name'])
+
+    def __check_status_code(self, api_response):
+        if api_response.status_code == 200:
+            print('Connection succesful')
+        else:
+            print(f'Error, could not connect. Status code {api_response.status_code}')
+
 
